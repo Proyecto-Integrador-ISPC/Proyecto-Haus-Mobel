91e0cf4d8119f99ac03c22aefd56e6467a4656f3
MiltonMoreno <mm0358554@gmail.com>
2024-05-09 20:52:14 -0300

    creacion del componente Footer

--- proyecto-haus-mobel/src/app/app.component.ts
+++ proyecto-haus-mobel/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './shared/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
