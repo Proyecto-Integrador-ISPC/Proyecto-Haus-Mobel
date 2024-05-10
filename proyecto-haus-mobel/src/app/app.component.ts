6c936772d7c26a61383fec839157c23c8dc5cf04
MiltonMoreno <mm0358554@gmail.com>
2024-05-10 12:38:19 -0300

    impotandocion del componente Container al proyecto

--- proyecto-haus-mobel/src/app/app.component.ts
+++ proyecto-haus-mobel/src/app/app.component.ts
@@ -3,12 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './shared/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FoorterComponent } from './shared/foorter/foorter.component';
-
+import { ContainerComponent } from './shared/container/container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, HomeComponent, FoorterComponent],
+  imports: [RouterOutlet, NavComponent, HomeComponent, FoorterComponent, ContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
