347b14d0ecba8202580f7d7825e490316b7fb8c1
MiltonMoreno <mm0358554@gmail.com>
2024-05-09 21:05:31 -0300

    importando footer al proyecto

--- proyecto-haus-mobel/src/app/app.component.ts
+++ proyecto-haus-mobel/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './shared/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
+import { FoorterComponent } from './shared/foorter/foorter.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, HomeComponent],
+  imports: [RouterOutlet, NavComponent, HomeComponent, FoorterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
