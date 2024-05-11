a3d4ced47ca91da958dec90a8c8526cf841b39ec
Milena Gimenez <milenanicolegimenez@gmail.com>
2024-05-10 22:41:50 -0300

    creando formulario

--- proyecto-haus-mobel/src/app/pages/loguin/loguin.component.ts
+++ proyecto-haus-mobel/src/app/pages/loguin/loguin.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-loguin',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './loguin.component.html',
   styleUrl: './loguin.component.css'
 })
-export class LoguinComponent {
 
+
+export class LoguinComponent {
+  // loguinFormControl = new FormControl('', []);
+  form!:FormGroup;
+  constructor(private formBuilder:FormBuilder)
+  {
+    this.form=this.formBuilder.group(
+      {
+        usuario:['',[]],
+        password:['',[]]
+      }
+    )
+    
+  }
+
+  onEnviar(event:Event)
+  {
+    console.log(this.form.value)
+  }
+    
 }
+
