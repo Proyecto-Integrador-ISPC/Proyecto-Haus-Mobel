762d77d4a0b569128a7d4c2ec1e5b256101e05c0
Milena Gimenez <milenanicolegimenez@gmail.com>
2024-05-12 23:14:48 -0300

    añadiendo funcionalidad al formulario de registro

--- proyecto-haus-mobel/src/app/pages/registro/registro.component.ts
+++ proyecto-haus-mobel/src/app/pages/registro/registro.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
 export class RegistroComponent {
+ form!:FormGroup;
+ constructor(private formBuilder:FormBuilder)
+ {
+   this.form=this.formBuilder.group(
+     {
+       usuario:['',[]],
+       email:['',[]],
+       password:['',[]]
+     }
+   )
+   
+ }
 
+ onEnviar(event:Event)
+ {
+   console.log(this.form.value)
+ }
+   
 }
