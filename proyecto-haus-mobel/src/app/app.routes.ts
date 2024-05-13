864ae98a8238e854ec1b2c43a032a4a2e3d5e579
Milena Gimenez <milenanicolegimenez@gmail.com>
2024-05-12 22:58:37 -0300

    añadiendo sistema de rutas

--- proyecto-haus-mobel/src/app/app.routes.ts
+++ proyecto-haus-mobel/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { LoguinComponent } from './pages/loguin/loguin.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", component:HomeComponent},
+    {path:"home", component:HomeComponent},
+    {path:"productos", component:ProductosComponent},
+    {path:"loguin", component:LoguinComponent}
+];
