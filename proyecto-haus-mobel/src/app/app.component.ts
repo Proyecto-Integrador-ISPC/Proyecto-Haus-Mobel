316f7a602df5a2625fe46b705accb504c9d47442
facundo_arias1998@hotmail.com <facundo_arias1998@hotmail.com>
2024-05-13 19:07:18 -0300

    importando contacto al proyecto y componente html

--- proyecto-haus-mobel/src/app/app.component.ts
+++ proyecto-haus-mobel/src/app/app.component.ts
@@ -4,13 +4,13 @@ import { NavComponent } from './shared/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FoorterComponent } from './shared/foorter/foorter.component';
 import { ProductosComponent } from './pages/productos/productos.component';
-
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 import { LoguinComponent } from './pages/loguin/loguin.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, HomeComponent, FoorterComponent, ProductosComponent, LoguinComponent],
+  imports: [RouterOutlet, NavComponent, HomeComponent, FoorterComponent, ProductosComponent, LoguinComponent, ContactoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
