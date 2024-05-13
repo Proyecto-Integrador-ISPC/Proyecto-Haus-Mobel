80b595d2c9bc55571a33c203290d23c5e734e7a6
Milena Gimenez <milenanicolegimenez@gmail.com>
2024-05-12 23:07:56 -0300

    creando componente registro

--- proyecto-haus-mobel/src/app/app.routes.ts
+++ proyecto-haus-mobel/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ProductosComponent } from './pages/productos/productos.component';
 import { LoguinComponent } from './pages/loguin/loguin.component';
 import { HomeComponent } from './pages/home/home.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
 export const routes: Routes = [
     {path:"", component:HomeComponent},
     {path:"home", component:HomeComponent},
     {path:"productos", component:ProductosComponent},
-    {path:"loguin", component:LoguinComponent}
+    {path:"loguin", component:LoguinComponent},
+    {path:"registro", component:RegistroComponent}
 ];
