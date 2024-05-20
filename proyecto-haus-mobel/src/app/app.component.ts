df182e65d0b27982f6ee5b97c419b9888be91528
Milena Gimenez <milenanicolegimenez@gmail.com>
2024-05-20 18:21:25 -0300

    correccion componente footer por error en el nombre del componente

--- proyecto-haus-mobel/src/app/app.component.ts
+++ proyecto-haus-mobel/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './shared/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
-import { FoorterComponent } from './shared/foorter/foorter.component';
+import { FooterComponent } from './shared/footer/footer.component';
 import { ProductosComponent } from './pages/productos/productos.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; 
 
@@ -11,7 +11,7 @@ import { LoguinComponent } from './pages/loguin/loguin.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, HomeComponent, FoorterComponent, ProductosComponent, LoguinComponent, NgbModule ],
+  imports: [RouterOutlet, NavComponent, HomeComponent, FooterComponent, ProductosComponent, LoguinComponent, NgbModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- proyecto-haus-mobel/src/app/shared/footer/footer.component.spec.ts
+++ proyecto-haus-mobel/src/app/shared/footer/footer.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FoorterComponent } from './foorter.component';
+import { FooterComponent } from './footer.component';
 
-describe('FoorterComponent', () => {
-  let component: FoorterComponent;
-  let fixture: ComponentFixture<FoorterComponent>;
+describe('FooterComponent', () => {
+  let component: FooterComponent;
+  let fixture: ComponentFixture<FooterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FoorterComponent]
+      imports: [FooterComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(FoorterComponent);
+    fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
