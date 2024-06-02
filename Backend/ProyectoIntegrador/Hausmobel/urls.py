a6e261f46e6fd9d558bfc369afb929c40f7ff85f
Mateo88XD <107517731+Mateo88XD@users.noreply.github.com>
2024-06-02 19:48:44 -0300

    Update urls.py

--- Backend/ProyectoIntegrador/Hausmobel/urls.py
+++ Backend/ProyectoIntegrador/Hausmobel/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 from rest_framework import routers
-<<<<<<< HEAD
 from Hausmobel import views
 
 router= routers.DefaultRouter()
@@ -9,14 +8,4 @@ router.register(r'usuario',views.UsuarioViewSet)
 urlpatterns = [
      path('', include(router.urls)),
 ]
-=======
-from views import UsuarioViewSet
 
-
-router= routers.DefaultRouter()
-router.register(r'aula',UsuarioViewSet)
-#----
-urlpatterns = [
-     path('', include(router.urls)),
-]
->>>>>>> 26770838a47d980cbffb40dcf51ccd043bc10ab3
