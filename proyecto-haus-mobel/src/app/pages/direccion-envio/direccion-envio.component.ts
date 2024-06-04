751880b01bc3a2ec7805a21758c7e7b2aca17269
Milena Gimenez <milenanicolegimenez@gmail.com>
2024-06-03 21:28:10 -0300

    creando plataforma de pago

--- proyecto-haus-mobel/src/app/pages/direccion-envio/direccion-envio.component.ts
+++ proyecto-haus-mobel/src/app/pages/direccion-envio/direccion-envio.component.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-direccion-envio',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './direccion-envio.component.html',
   styleUrl: './direccion-envio.component.css'
 })
 export class DireccionEnvioComponent {
-  constructor(private router: Router){}
+  form!:FormGroup;
+  constructor(private router: Router, private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      address: ['', Validators.required],
+      numeracion: ['', Validators.required],
+      ciudad: ['', Validators.required],
+      pais: ['', Validators.required],
+      telefono: ['', Validators.required],
+    });
+  }
 
+  onSubmit() {
+    if (this.form.valid) {
+      alert("Mensaje enviado exitosamente.")
+    } else {
+      alert("Por favor, completá todos los campos antes de enviar el mensaje.")
+    }
+  }
 }
